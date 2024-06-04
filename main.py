163595e48ddbcea56c97e9dc28a0c5b505275c36
ianoliveirasilva <ianoliveira.tec@gmail.com>
2024-06-04 15:27:59 -0300

    Adding port

--- main.py
+++ main.py
@@ -31,6 +31,5 @@ def main():
 
     return render_template('index.html', show_image=False)
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
